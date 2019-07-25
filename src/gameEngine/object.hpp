0c67e9ceae85913b92c4289b1c1e2bbeb010e3f3
ZiperZ <53088164+ZiperZTheUnbeaten@users.noreply.github.com>
2019-07-25 14:12:51 +0300

    Update object.hpp

--- src/gameEngine/object.hpp
+++ src/gameEngine/object.hpp
@@ -1,15 +1,44 @@
 #ifndef ZIPERZ_OBJECT_HPP
 #define ZIPERZ_OBJECT_HPP
 
-#include "transform.hpp"
+#include "componentManager.hpp"
+
+#include <functional>
+#include <vector>
 
 namespace zl
 {
-	class object
+	using onDestroyFunction = void(*)(zl::object*);
+	
+	class object : componentManager
 	{
 	public:
+		object() = delete;
+		object(const object& obj);
+		explicit object(size_t objectID, const std::string& name);
+		~object();
+		
+		// # Returns true if objects have same properties
+		bool operator==	(const object& obj);
+		
+		// # Duplicates object (objects gonna still keep their unique IDs)
+		void operator=	(const object& obj);
+		
+		// # Clones object and returns pointer of new instance
+		zl::object* cloneObject();
+		
+		// # Returns objects unique ID
+		size_t getObjectID();
+		void destroy();
+		
+		// # Contains objects position, rotation and scale
 		zl::transform transform;
+		
+		// # Objects non-unique name
+		std::string name;
 	private:
+		std::function<onDestroyFunction> m_onDestroyFunction;
+		size_t m_objectID;
 	};
 }
 
