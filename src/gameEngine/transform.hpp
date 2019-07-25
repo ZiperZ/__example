6b41c2b336ae93c66f3f9b6198ab2b53134fd761
ZiperZ <53088164+ZiperZTheUnbeaten@users.noreply.github.com>
2019-07-25 14:24:51 +0300

    Update transform.hpp

--- src/gameEngine/transform.hpp
+++ src/gameEngine/transform.hpp
@@ -1,20 +1,21 @@
 #ifndef ZIPERZ_TRANSFORM_HPP
 #define ZIPERZ_TRANSFORM_HPP
 
-#include <glm/glm.hpp>
+#include <reactphysics3d.h>
 
 namespace zl
 {
-	using position	= glm::vec3;
-	using rotation	= glm::vec3;
-	using scale		= glm::vec3;
-	
 	class transform
 	{
 	public:
-		position	position;
-		rotation	rotation;
-		scale		scale;
+		transform() = default;
+		
+		void translate(rp3d::Vector3 vec);
+		void rotate(rp3d::Vector3 vec);
+		
+		rp3d::Vector3 position;
+		rp3d::Vector3 rotation;
+		rp3d::Vector3 scale;
 	};
 }
 
