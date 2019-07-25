d62fb51b1c4ae8629957d7947a6cf47298f061e9
ZiperZ <53088164+ZiperZTheUnbeaten@users.noreply.github.com>
2019-07-25 14:12:47 +0300

    Update ziperz.hpp

--- src/gameEngine/ziperz.hpp
+++ src/gameEngine/ziperz.hpp
@@ -5,10 +5,30 @@
 
 namespace zl
 {
-	void createObject(const zl::object& value = {});
-	void deleteObject(const zl::object& value = {});
-
-	int  getObjectInLua();
+	namespace debug
+	{
+		enum class messageType
+		{
+			PRINT_MESSAGE,
+			PRINT_WARNING,
+			PRINT_ERROR
+		};
+		
+		void printMessage	(const std::string& message, messageType type);
+		void printWarning	(const std::string& message);
+		void printError		(const std::string& message);
+	}
+	
+	class gameEngine
+	{
+		zl::object* 	createObject();
+		void 			deleteObject(const zl::object* obj);
+	
+		zl::object* 	findObjectByID(size_t objectID);
+	
+		// # Get reference to object array that created by Ziperian Library
+		std::vector<zl::object>& getObjectList();
+	};
 }
 
 #endif // end header guard
